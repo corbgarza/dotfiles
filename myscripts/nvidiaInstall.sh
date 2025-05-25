13eaa7cfc281f5a7cbce59f7d36c99fe6d77d1d3
corbgarza <garza20@protonmail.com>
2025-05-24 19:22:12 -0500

    update

---
+++ myscripts/nvidiaInstall.sh
@@ -0,0 +1,4 @@
+sudo apt install -y linux-headers-amd64
+sudo sed -i*.old's/trixie main/trixie main contrib non-free non-free-firmware/1' /etc/apt/sources.list
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y nvidia-kernel-dkms nvidia-driver firmware-misc-nonfree
