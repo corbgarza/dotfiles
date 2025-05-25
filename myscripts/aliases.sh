550b80ee99dfcbc751b5d83aa71fc7ed2882ccb7
corbgarza <garza20@protonmail.com>
2025-05-24 22:46:50 -0500

    update

--- myscripts/aliases.sh
+++ myscripts/aliases.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-alias clickclass="xprop | grep WM_CLASS | awk '{ print \$4 }'"
 alias config-i3="nvim ~/.config/i3/config"
 alias config-aliases="nvim ~/aliases.sh"
 alias config-kitty="nvim ~/.config/kitty/kitty.conf"
@@ -10,15 +9,17 @@ alias config-qute="nvim ~/.config/qutebrowser/config.py"
 alias config-rofi="nvim ~/.config/rofi/config.rasi"
 alias config-sxhkd="nvim ~/.config/sxhkd/sxhkdrc"
 alias config-zsh="nvim ~/.zshrc"
+
+alias clickclass="xprop | grep WM_CLASS | awk '{ print \$4 }'"
 alias destroy="sudo apt purge -y "
 alias ff="fastfetch"
 alias i="sudo apt install -y "
-alias la="ls --almost-all --color=always"
-alias lla="ls --almost-all --human-readable --color=always -l"
-alias ll="ls -l --human-readable --color=always"
-alias ls="ls --color=always"
-alias rebootnow="sudo reboot now"
-alias shutdownnow="sudo shutdown now"
+alias la="eza -a"
+alias lla="eza -la"
+alias ll="eza -l"
+alias ls="eza"
+alias rnow="sudo reboot now"
+alias snow="sudo shutdown now"
 alias v="nvim"
 
 #git add, commit, and push
@@ -28,43 +29,27 @@ gacp () {
 	git push
 }
 
-#git pulls
-gitpulls () {
-	cd ~/projects && git pull && echo '\n#### projects pulled successfully ####\n'
-	cd ~/dotfiles && git pull && echo '\n#### dotfiles pulled successfully ####\n'
-	cd ~/Wallpaper && git pull && echo '\n#### Wallpapers pulled successfully ####\n'
-	cd ~
-}
-
-#git pushes
-gitpushes () {
-	cd ~/projects && gacp && echo '\n#### projects pushed successfully ####\n' 
-	cd ~/dotfiles && gacp && echo '\n#### dotfiles pushed successfully ####\n'
-	cd ~/Wallpaper && gacp && echo '\n#### Wallpapers pushed successfully ####\n'
-	cd ~
-}
-
 #search files
 search () {
 	cd /
 	sudo fzf -e
-cd -
+	cd -
 }
 
 #apt update, apt upgrade, apt autoremove, apt autoclean, tldr update, brew update, flatpak update
 updater () {
-	sudo apt update -y && sudo apt upgrade -y && echo -e "\n#### apt upgraded ####\n"
-	sudo apt autoremove -y && sudo apt autoclean -y && echo -e "\n#### autoremoved and autocleaned ####\n"
-	tldr --update && echo -e "\n#### tldr updated ####\n"
-	brew update && brew upgrade && echo -e "\n#### brew upgraded ####\n"
-	flatpak update && echo -e "\n#### flatpak upgraded ####\n"
+	sudo apt update -y && sudo apt upgrade -y
+	sudo apt autoremove -y && sudo apt autoclean -y
+	tldr --update
+	brew update && brew upgrade
+	flatpak update
 }
 
 #search pkgs
 pkgsearch () {
-	apt search $1 ; echo -e "\n#### apt results completed ####\n"
-	brew search $1 ; echo -e "\n#### brew results completed ####\n"
-	flatpak search $1 ; echo -e "\n#### flatpak results completed ####\n"
+	apt search $1 && echo -e "\n#### apt results completed ####\n"
+	brew search $1 && echo -e "\n#### brew results completed ####\n"
+	flatpak search $1 && echo -e "\n#### flatpak results completed ####\n"
 }
 
 #yt-dlp termux
--- myscripts/autoInstall.sh
+++ myscripts/autoInstall.sh
@@ -1,38 +1,32 @@
-sudo apt install -y brightnessctl btop curl fastfetch feh ffmpeg fonts-noto-color-emoji fzf gettext git i3 imagemagick kitty lightdm mpv network-manager nm-tray openssh-client openssh-server picom pipx polybar psmisc pulseaudio python3 ripgrep rofi stow sxhkd tealdeer x11-xserver-utils xsel zoxide zsh
+sudo apt install -y brightnessctl btop curl eza fastfetch feh ffmpeg fonts-noto-color-emoji fzf gcc git i3 imagemagick kitty mpv network-manager nm-tray openssh-client openssh-server picom pipx polybar psmisc pulseaudio python3 ripgrep rofi stow sxhkd startx tealdeer x11-xserver-utils xsel zoxide zsh
+
 pipx install pywal16 spotdl yt-dlp
 
 sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
-zsh -c "brew install bat nvim yazi"
+/home/linuxbrew/.linuxbrew/bin/brew install bat nvim yazi
 
 curl -fsS https://dl.brave.com/install.sh | sh
+
 git clone https://github.com/corbgarza/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 git clone https://github.com/corbgarza/dotfiles.git $HOME/dotfiles 
-git clone https://github.com/corbgarza/projects.git $HOME/projects
 git clone https://github.com/corbgarza/Wallpaper.git $HOME/Wallpaper
-if [[ ! -e ~/repos/powerlevel10k ]]; then git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/repos/powerlevel10k; echo "installing powerlevel10k"; fi
-if [[ ! -e ~/repos/zsh-syntax-highlighting ]]; then git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/repos/zsh-syntax-highlighting; echo "installing syntax-highlighting"; fi 
-if [[ ! -e ~/repos/zsh-autosuggestions ]]; then git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/repos/zsh-autosuggestions; echo "installing autosuggestions"; fi
-if [[ ! -e ~/dotfiles ]]; then git clone https://github.com/corbgarza/dotfiles $HOME/dotfiles; echo "installing dotfiles"; fi
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/repos/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/repos/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/repos/zsh-autosuggestions
 
 cd $HOME/dotfiles
-stow .
+stow *
 sudo cp $HOME/dotfiles/fonts/* /usr/local/share/fonts/
-
-cd $HOME/dotfiles
+rm $HOME/*.ttf
 git remote set-url origin git@github.com:corbgarza/dotfiles.git
-cd $HOME/projects
-git remote set-url origin git@github.com:corbgarza/projects.git
-cd $HOME/Wallpaper
-git remote set-url origin git@github.com:corbgarza/Wallpaper.git
 
 cd $HOME
 mkdir -p $HOME/.ssh
 ssh-keygen -f $HOME/.ssh/github
 
-sudoedit /etc/default/grub
-echo 'export PATH=$PATH:$HOME' > $HOME/.xsessionrc
+sudo sed -i*.old 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/1' /etc/default/grub
+sudo update-grub
+echo 'export PATH=$PATH:$HOME' > $HOME/.xinitrc
 
 chsh -s $(which zsh)
 sudo update-alternatives --config editor
-sudo update-alternatives --config x-terminal-emulator
-
