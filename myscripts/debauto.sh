24ba9a62e5654e96ffd6b9cd9fc9beb14a9dbd17
corbgarza <garza20@protonmail.com>
2025-06-15 19:04:43 -0400

    update

--- myscripts/debauto.sh
+++ myscripts/debauto.sh
@@ -1,32 +1,21 @@
 git remote set-url origin git@github.com:corbgarza/dotfiles.git
-sudo apt install -y curl
 source $HOME/dotfiles/myscripts/progs.csv
-mkdir -p $HOME/.config 
-gitpath="$HOME/.config/"
-
-for prog in $(cat progs.csv); do
-	awk '{print $1}' prog > PKGMAN
-	sudo apt install -y $prog
-#if b, then brew
-	brew install $prog
-#if p, then pip
-	pipx install $prog
-#if g, git clone --depth=1
-	git clone --depth=1 https://github.com/$prog $gitpath
-done
-
-sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+mkdir -p $HOME/.config && gitpath="$HOME/.config/"
 
 curl -fsS https://dl.brave.com/install.sh | sh
-
-"${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
-
 stow -d $HOME/dotfiles -t $HOME --ignore=*.ttf
+sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
 sudo cp $HOME/dotfiles/fonts/* /usr/local/share/fonts/
-
 sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
 sudo update-grub
+while IFS=, read -r installer package; do
+	case "$installer" in
+	"a") sudo apt install -y "$package" ;;
+	"b") brew install -y "$package" ;;
+	"p") pipx install "$package" ;;
+	"g") git clone --depth=1 "$package" $HOME/.config/$package ;;
+	esac
+done <progs.csv
 sudo echo "export EDITOR=$(which nvim)" >> /root/.profile
 echo 'export PATH=$PATH:$HOME' >> $HOME/.xinitrc
-
 chsh -s $(which zsh)
