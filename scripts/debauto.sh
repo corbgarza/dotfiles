bf71d6c27abca8985340a4d6b8bd8bbad5a4a4fb
corbgarza <garza20@protonmail.com>
2025-07-07 09:11:54 -0400

    update

--- scripts/debauto.sh
+++ scripts/debauto.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo apt install -y nala git curl
+[[ command -v nala ]] || echo "Install curl, git and nala!"; exit 1
 git clone https://github.com/corbgarza/dotfiles.git $HOME/dotfiles.git
 cd $HOME/dotfiles
 git remote set-url origin git@github.com:corbgarza/dotfiles.git
@@ -11,16 +11,17 @@ while IFS=, read -r installer package; do
   gitpath=$HOME/.config/$(echo $package | awk -F "." '{print $(NF-1)}')
   n=$((n + 1))
 	case "$installer" in
-	"a") sudo apt install -y $package > /dev/null && echo "Installed $package, #$n of $total!" ;;
+	"a") sudo nala install -y $package > /dev/null && echo "Installed $package, #$n of $total!" ;;
 	"b") /home/linuxbrew/.linuxbrew/bin/brew install $package > /dev/null && echo "Installed $package, #$n of $total!";;
 	"p") pipx install $package > /dev/null && echo "Installed $package, #$n of $total!" ;;
 	"g") git clone --depth=1 https://github.com/$package $gitpath > /dev/null && echo "Installed $package, #$n of $total!" ;;
 	esac
 done <$HOME/dotfiles/scripts/debauto.progs
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher; echo "Installed Fisher!"
-stow -t $HOME -d $HOME/dotfiles && echo "Stowed Dotfiles!"
 sudo cp $HOME/dotfiles/fonts/* /usr/local/share/fonts/ && echo "Copied Fonts!"
 sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub && sudo update-grub
 sudo echo "export EDITOR=$(which nvim)" >> /root/.profile && echo "Nvim is root EDITOR!"
 sudo echo "$(which fish)" >> /etc/shells && echo "Added fish to shells!"
 chsh -s $(which fish) && echo "Changed default shell to fish!"
+echo "Stow dotfiles and then reboot to complete setup!"
+cd $HONE/dotfiles
