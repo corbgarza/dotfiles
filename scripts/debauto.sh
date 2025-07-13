82b0dcc2e7b5ccaf659dad4f7c1788e591fd8d22
corbgarza <garza20@protonmail.com>
2025-07-13 10:45:26 -0500

    update

--- scripts/debauto.sh
+++ scripts/debauto.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-sudo apt install -y nala git >/dev/null
+cat ~/.pass | sudo -S apt install -y nala git >/dev/null && echo "Installed nala and git!"
 git clone https://github.com/corbgarza/dotfiles.git $HOME/dotfiles >/dev/null && echo "Cloned dotfiles!"
 #curl -fsS https://dl.brave.com/install.sh | sh > /dev/null && echo "Installed Brave!"
-sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash >/dev/null && echo "Installed Brew!"
+cat ~/.pass | sudo -S curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash >/dev/null && echo "Installed Brew!"
 total=$(wc -l <$HOME/dotfiles/scripts/debauto.progs)
-echo "Total packages to install: $total"
 while IFS=, read -r installer package; do	
   gitpath=$HOME/.config/$(echo $package | awk -F "." '{print $(NF-1)}')
   n=$((n + 1))
@@ -18,11 +17,10 @@ while IFS=, read -r installer package; do
 done <$HOME/dotfiles/scripts/debauto.progs
 cd $HOME/dotfiles && git remote set-url origin git@github.com:corbgarza/dotfiles.git >/dev/null && echo "Set dotfiles url!"
 mkdir -p $HOME/.config
-stow -t ~/.config -d ~/dotfiles
-sudo cp $HOME/dotfiles/fonts/* /usr/local/share/fonts/ && echo "Copied Fonts!"
-sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub && sudo update-grub > /dev/null && echo "Updated Grub!"
+cat ~/.pass | sudo -S cp $HOME/dotfiles/fonts/* /usr/local/share/fonts/ && echo "Copied Fonts!"
+cat ~/.pass | sudo -S sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub && sudo update-grub > /dev/null && echo "Updated Grub!"
 #sudo echo "export EDITOR=$(which nvim)" >> /root/.profile && echo "root EDITOR is set to nvim!"
-echo "$(which fish)" >> /etc/shells && echo "Added fish to /etc/shells!"
+cat ~/.pass | sudo -S echo "$(which fish)" >> /etc/shells && echo "Added fish to /etc/shells!"
 chsh -s $(which fish) && echo "Changed default shell to fish!"
 fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"; echo 'Installed Fisher!'
 echo "Done! Reboot to complete!"
