e01ca6dd3eb185c8a81303fcb4a39a4a87948172
corbgarza <garza20@protonmail.com>
2025-10-19 10:01:48 -0500

    update

--- scripts/archauto.sh
+++ scripts/archauto.sh
@@ -1,11 +1,9 @@
-pkg_array=("bat brightnessctl btop curl eza fd ffmpeg fish fzf gcc git imagemagick kitty man-db mpv neovim networkmanager openssh pipewire python3 qutebrowser ripgrep stow tealdeer tree udiskie vlc waybar wireplumber wofi yt-dlp zoxide")
+pkg_array=("bat brightnessctl btop curl eza fd ffmpeg fish fzf gcc git imagemagick kitty man-db mpv neovim networkmanager noto-fonts openssh otf-font-awesome pipewire python3 qutebrowser ripgrep stow tealdeer tree ttf-arimo-nerd udiskie vlc waybar wireplumber wofi yt-dlp zoxide")
 
 sudo pacman -S pkg_array[@]
 
 git clone https://github.com/corbgarza/dotfiles
-git clone https://github.com/corbgarza/projects
 
 stow fish gitconfig hypr kitty nvim qutebrowser waybar yt-dlp
 
 cd $HOME/dotfiles && git remote set-url origin git@github.com:corbgarza/dotfiles
-cd $HOME/projects && git remote set-url origin git@github.com:corbgarza/projects
