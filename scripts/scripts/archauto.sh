ee618c64c1a7326498f0db7d7b337f436c467ca0
corbgarza <garza20@protonmail.com>
2025-11-01 18:58:09 -0500

    script updater

--- scripts/scripts/archauto.sh
+++ scripts/scripts/archauto.sh
@@ -1,4 +1,4 @@
-pkg_array=("bat" "brightnessctl" "btop" "curl" "eza" "fastfetch" "fd" "ffmpeg" "fish" "fzf" "gcc" "git" "imagemagick" "kitty" "man-db" "mpv" "neovim" "networkmanager" "noto-fonts" "os-prober" "openssh" "otf-font-awesome" "pulseaudio" "python3" "qutebrowser" "ripgrep" "stow" "tealdeer" "tree" "ttf-arimo-nerd" "udiskie" "vlc" "waybar" "wireplumber" "wofi" "yazi" "yt-dlp" "zoxide")
+pkg_array=("bat" "brightnessctl" "btop" "curl" "eza" "fastfetch" "feh" "fd" "ffmpeg" "fish" "fzf" "gcc" "git" "imagemagick" "kitty" "man-db" "mpv" "neovim" "networkmanager" "noto-fonts" "os-prober" "openssh" "otf-font-awesome" "pulseaudio" "python3" "qutebrowser" "ripgrep" "stow" "tealdeer" "tree" "ttf-arimo-nerd" "udiskie" "vlc" "waybar" "wireplumber" "wofi" "yazi" "yt-dlp" "zoxide")
 
 sudo pacman -S ${pkg_array[@]}
 
