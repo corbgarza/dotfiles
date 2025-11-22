36d479d28ec749ef09e332f134150a8df24b5e63
corbgarza <garza20@protonmail.com>
2025-11-21 18:53:53 -0600

    no idea

--- hypr/.config/hypr/change_wallpaper.sh
+++ hypr/.config/hypr/change_wallpaper.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-while true; do
-		pkill swaybg ; swaybg -i $(find ~/Walls/. -type f | shuf -n1) -m fit &
-		sleep 60
-done
+pkill swaybg ; swaybg -i $(find ~/Walls/. -type f | shuf -n1) -m fit &
