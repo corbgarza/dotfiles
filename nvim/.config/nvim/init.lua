02fdc91637c330f65565bf8225efc4167cc5d374
corbgarza <garza20@protonmail.com>
2026-06-17 21:19:25 -0500

    lil updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -64,8 +64,6 @@ spec = {
 				{ "ms-jpq/coq_nvim", branch = "coq" }, 
 				{ "ms-jpq/coq.artifacts", branch = "artifacts" },
 				},
-				  init = function()
-				    vim.g.coq_settings = { auto_start = 'shut-up' }end,
 				},
 },
 rocks = {enabled = false},
