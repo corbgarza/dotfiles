abae55a17b3a5b8b30a49f7dc5f1bce31b3af451
corbgarza <garza20@protonmail.com>
2026-07-02 21:13:40 -0500

    updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,7 +7,8 @@ set -gx EDITOR $(which nvim)
 set -gx VISUAL $(which nvim)
 set -U fish_greeting ""
 set -gx PATH $PATH:$HOME/.local/bin:$HOME/.config/fish
-set -gx SSH_AUTH_SOCK "$PREFIX/var/run/ssh-agent.socket"
+#set -gx SSH_AUTH_SOCK "$PREFIX/var/run/ssh-agent.socket"
+eval $(ssh-agent -c) > /dev/null
 
 alias cfish="nvim ~/.config/fish/config.fish"
 alias chypr="nvim ~/.config/hypr/hyprland.conf"
@@ -17,9 +18,5 @@ alias czsh="nvim ~/.zshrc"
 alias fzfl="sudo fzf -e -i -m --wrap --preview='bat {}' "
 alias fzft="fzf -e -i -m --wrap --preview='bat {}'"
 alias gacp="git add -A && git commit && git push"
-alias la="eza -a --color=always"
-alias lla="eza -la --color=always"
-alias ll="eza -l --color=always"
-alias ls="eza --color=always"
 alias sfish="source ~/.config/fish/config.fish"
 alias treee="tree -a -C -I '.git'"
