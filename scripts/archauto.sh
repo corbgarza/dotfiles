ee46952fdf4ba6709b8d884ae2cdccdf2374a4c9
corbgarza <garza20@protonmail.com>
2026-07-03 20:36:23 -0500

    clean up

--- scripts/archauto.sh
+++ scripts/archauto.sh
@@ -1,10 +1,9 @@
-pkg_array=("bat" "brightnessctl" "btop" "curl" "deno" "eza" "fastfetch" "feh" "fd" "ffmpeg" "fish" "fzf" "gcc" "git" "imagemagick" "kitty" "man-db" "mpv" "neovim" "networkmanager" "nmap" "noto-fonts" "os-prober" "openssh" "otf-font-awesome" "powertop" "pulseaudio" "python3" "qutebrowser" "rclone" "resolvconf" "ripgrep" "stow" "swaybg" "tealdeer" "traceroute" "tree" "ttf-jetbrains-mono-nerd" "tmux" "udiskie" "vlc" "waybar" "wireplumber" "wofi" "yazi" "yt-dlp" "zoxide")
+pkg_array=("bat" "brightnessctl" "curl" "deno" "dolphin" "dunst" "eza" "fastfetch" "feh" "fd" "ffmpeg" "fish" "fzf" "gcc" "git" "htop" "imagemagick" "kitty" "ly" "man-db" "moonlight" "neovim" "networkmanager" "nmap" "opendoas" "openssh" "os-prober" "pulseaudio" "python3" "qutebrowser" "rclone" "resolvconf" "ripgrep" "stow" "swaybg" "swaylock" "systemd-resolvconf" "tailscale" "tealdeer" "traceroute" "tree" "ttf-jetbrains-mono-nerd" "tmux" "udiskie" "unzip" "vlc" "waybar" "wayclip" "wget" "wofi" "yazi" "yt-dlp" "zip" "zoxide")
 
 sudo pacman -S ${pkg_array[@]}
 
 chsh -s $(which fish)
 git clone https://github.com/corbgarza/dotfiles
-#git clone git@github.com:corbgarza/dotfiles
 rm -rf ~/.config/hypr
 stow fish gitconfig hypr kitty nvim tealdeer waybar yt-dlp
 cd $HOME/dotfiles && git remote set-url origin git@github.com:corbgarza/dotfiles
